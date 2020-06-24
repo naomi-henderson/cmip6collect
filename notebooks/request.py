1cc68bb0b86e5bef692c3c8740edb2c98e352853
naomi-henderson <31460695+naomi-henderson@users.noreply.github.com>
2020-06-24 13:54:15 -0400

    added a few more comments

--- notebooks/request.py
+++ notebooks/request.py
@@ -36,8 +36,6 @@ def getsheet():
     return df
 
 def requests(df_prior,rows=[],emails=[],tables=[]): 
-    json_keyfile = 'json/Pangeo Hackathon-e48a41b13c91.json'
-    sheet_name = "CMIP6 GCS Data Request (Responses)"
 
     df = df_prior.copy()
     
